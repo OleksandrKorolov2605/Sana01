b6076dc9161ac6cdcf08c35e85c63b9850bf4f07
OlexandrKorolov <alexandrkorolov2605@gmail.com>
2025-11-08 19:50:26 +0200

    Added second linear expression

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -3,7 +3,7 @@
     static void Main(string[] args)
     {
         double a, b, c, d;
-        
+
         Console.Write("Enter the value of a: ");
         a = double.Parse(Console.ReadLine());
         Console.Write("Enter the value of b: ");
@@ -19,7 +19,7 @@
         y = ((5 * (a + b) * (c - d)) / ((1.0 / 2) * c)) + Math.Pow(d, 2) * (((Math.Pow(a, 2)) - (Math.Pow(b, 2))) / (b - a));
         z = (((Math.Pow((Math.Pow(x, 2) - 2 * x), 3)) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b)) / (5 * a + 3 * b);
         r = ((((1.0 / 2) * a) + ((3.0 / 4) * b) - (7.0 / 5)) / (3 * c) + 1) + (1 / (a - c));
-        
+
         Console.WriteLine($"x = {x}");
         Console.WriteLine($"y = {y}");
         Console.WriteLine($"z = {z}");
---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double m, n;
+        double z1, z2;
+        
+        Console.Write("Enter the value of m: ");
+        m = double.Parse(Console.ReadLine());
+        Console.Write("Enter the value of n: ");
+        n = double.Parse(Console.ReadLine());
+        
+        z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(Math.Pow(m,3) * n) + n * m + Math.Pow(m,2) - m);
+        z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+        
+        Console.WriteLine($"z1 = {z1}");
+        Console.WriteLine($"z2 = {z2}");
+    }
+}
