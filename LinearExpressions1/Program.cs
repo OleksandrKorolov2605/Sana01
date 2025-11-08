957b44d38ec5c6f01ef357caaaaf9c6f2991ae8b
OlexandrKorolov <alexandrkorolov2605@gmail.com>
2025-11-08 19:15:22 +0200

    Added the ability to enter data type numbers double

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -2,6 +2,15 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        double a, b, c, d;
+        
+        Console.Write("Enter the value of a: ");
+        a = double.Parse(Console.ReadLine());
+        Console.Write("Enter the value of b: ");
+        b = double.Parse(Console.ReadLine());
+        Console.Write("Enter the value of c: ");
+        c = double.Parse(Console.ReadLine());
+        Console.Write("Enter the value of d: ");
+        d = double.Parse(Console.ReadLine());
     }
 }
